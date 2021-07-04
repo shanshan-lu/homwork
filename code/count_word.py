d49a5198b79821ec7d59d9c94e5ef8bba35a5a8f
shanshan-lu <lss1947608803@163.com>
2021-07-04 21:14:54 +0800

    Signed-off-by: shanshan-lu <lss1947608803@163.com>

--- code/count_word.py
+++ code/count_word.py
@@ -17,3 +17,8 @@ if __name__ == "__main__":
     filepath = "../doc/english.txt"
     content = open_file(filepath)
     count_word_in_file(content)
+
+'''大文件的读取方式
+1、f.read(参数)通过参数指定每次读取的长度
+2、f.readline()每次读取一行
+3、分块读取：将大文件分割成小文件，然后每次读完后释放内存。'''
