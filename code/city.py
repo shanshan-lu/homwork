1fd522b529d533aa9e0ed0659b0330c6c0efd48b
shanshan-lu <lss1947608803@163.com>
2021-07-04 09:25:35 +0800

    kiss

--- code/city.py
+++ code/city.py
@@ -4,14 +4,18 @@
 import json
 import pandas as pd
 
-def transfer_txt_to_xlsx(filepath):
+def open_file_to_dict(filepath):
     with open(filepath,'r',encoding= "utf-8") as f:
-        data = f.read() 
-        convert_line = json.loads(data)  #将文本类型转化为字典
-        result = pd.DataFrame(convert_line.values(),index = convert_line.keys())
-        xlsx_file = result.to_excel('city.xlsx',header = False)
+        data = f.read()
+        dictionary = json.loads(data)  #将文本类型转化为字典 
+    return dictionary 
+
+def change_txt_to_xlsx(content:dict):
+    dataframe = pd.DataFrame(content.values(),index = content.keys())
+    xlsx_file = dataframe.to_excel('city.xlsx',header = False)
     return xlsx_file
     
 if __name__ =="__main__":
-    filepath = "E:\\LSS\\practice\\city.txt"
-    transfer_txt_to_xlsx(filepath)
+    filepath = "../doc/city.txt"
+    content = open_file_to_dict(filepath)
+    change_txt_to_xlsx(content)
