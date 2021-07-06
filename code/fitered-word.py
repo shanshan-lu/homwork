51c15eb80edcb629f8828108bc0c24fde6182617
shanshan-lu <lss1947608803@163.com>
2021-07-06 22:15:39 +0800

    改动

--- code/fitered-word.py
+++ code/fitered-word.py
@@ -1,4 +1,4 @@
-
+'''当用户输入敏感词语时，则打印出 Freedom，否则打印出 Human Rights。'''
 def open_file(filepath):
     senstivite_word = []
     with open(filepath,'r',encoding='utf-8') as f:
@@ -9,18 +9,31 @@ def open_file(filepath):
 def user_input():
     input_word = input('请输入你想搜索的内容：')
     return input_word
-
+'''
 def filtered_word(senstivite_word,input_word):
     for word in senstivite_word:
-        if input_word.find(word) == -1:
-           out_string = 'Human Rights'
+        if input_word.find(word) != -1:
+           out_string = 'Freedom'
+           return out_string
         else:
-            out_string = 'Freedom'
-        return out_string
+            out_string = 'Human Rights'
+    return out_string
 
 def display(out_string):
     print(out_string)
+'''
+def filtered_word(senstivite_word,input_word):
+    flag = 0 
+    for word in senstivite_word:
+        if input_word.find(word) != -1:
+          flag = 1
+    return flag
 
+def display(flag):
+    if flag == 0:
+        print('Human Rights')
+    else:
+        print('Freedom')
 
 if __name__ == '__main__':
     filepath = "../practice/doc/filtered_words.txt"
--- code/practice12.py
+++ code/practice12.py
@@ -1,26 +1,35 @@
-import re
+'''当用户输入敏感词语，则用 星号 * 替换，例如当用户输入「北京是个好城市」，则变成「**是个好城市」。'''
 
 def open_file(filepath):
+    senstivite_word = []
     with open(filepath,'r',encoding='utf-8') as f:
-        content = f.read()
-    return content
-    
+        for line in f.readlines():
+            senstivite_word.append(line.strip())
+    return senstivite_word
 
-def check_filtered_words(filtered_words,input):
-    result = re.match(input,filtered_words,flags=0)  #匹配对象，若是匹配成功返回匹配的对象，若是不成功返回None
-    return result
+def user_input():
+    input_word = input('请输入你想搜索的内容：')
+    return input_word
+
+def filtered_word(senstivite_word,input_word):
+    for word in senstivite_word:
+        if word in input_word:
+            input_word = input_word.replace(word,'*'*len(word))
+    return input_word
+
+def display(out_string):
+    print(out_string)
 
 
-def display(result):
-    if result != None:
-        print("Freedom")
-    else:
-        print("Human Rights")
-    
-
-if __name__ =="__main__":
+if __name__ == '__main__':
     filepath = "../practice/doc/filtered_words.txt"
-    word = input("输入您想搜索的词： ")
-    filtered_words =open_file(filepath)
-    result = check_filtered_words(filtered_words,word)
-    display(result)
+    content = open_file(filepath)
+    input_word = user_input()
+    out_string = filtered_word(content,input_word)
+    display(out_string)
+    
+    
+
+
+    
+
